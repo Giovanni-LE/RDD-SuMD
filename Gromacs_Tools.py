0e6b64aae84a18c7aec0f077ff4bf8c214fa3036
Giovanni-LE <119749266+Giovanni-LE@users.noreply.github.com>
2023-04-29 12:57:44 +0200

    Update Gromacs_Tools.py

--- Gromacs_Tools.py
+++ Gromacs_Tools.py
@@ -262,11 +262,11 @@ def g_make_ndx(f="conf.gro",n=0,o="index.ndx",Sel="\n"):
         echo = subprocess.Popen(cmdecho, stdout=subprocess.PIPE)
         process = subprocess.run(cmd,stdout=glog,stderr=glog,stdin=echo.stdout)  
 
-def g_energy(f,o):
+def g_pdb2gmx(pdbFile, TopolFile, groFile):
     if not os.path.exists('log'):
         os.mkdir('log')  
 
-    with open('log/energy.log', 'w') as glog:
-        cmd=['gmx' ,'energy','-f',f,'-o',o]
+    with open('log/pdb2gmx.log', 'w') as glog:
+        cmd=['gmx' ,'pdb2gmx','-f',pdbFile,'-o',groFile,'-p',TopolFile]
         cmd=[el for el in cmd if el != '']
         process = subprocess.run(cmd,stdout=glog,stderr=glog)
